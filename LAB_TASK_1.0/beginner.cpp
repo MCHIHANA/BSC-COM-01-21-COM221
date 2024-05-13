0df153f1e5ef63205b91aa84f397c9f9c8ee973b
MCHIHANA <bsc-com-01-21@unima.ac.mw>
2024-05-14 01:44:02 +0200

    C++ LIVE-ASSESSMENT-LAB SESSION

---
+++ LAB_TASK_1.0/beginner.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+	int valueA = 25;
+	vector<string> msg {"hello", "C++", "world", "programming"}
+	int valueB = 100;
+}
